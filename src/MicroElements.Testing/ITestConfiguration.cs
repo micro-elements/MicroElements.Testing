106cc1c42927fd6d56e8d881ca734e2dfce99323
Alexey Petriashev <petriashev@gmail.com>
2018-10-17 18:56:45 +0300

    test configuration changed to align StartupConfiguration from MicroElements

--- src/MicroElements.Testing/ITestConfiguration.cs
+++ src/MicroElements.Testing/ITestConfiguration.cs
@@ -6,8 +6,14 @@ namespace MicroElements.Testing
     public interface ITestConfiguration
     {
         /// <summary>
-        /// Gets environment.
+        /// Gets or sets configuration path. Can be relative or absolute.
         /// </summary>
-        string Environment { get; set; }
+        string ConfigurationPath { get; set; }
+
+        /// <summary>
+        /// Gets or sets configuration profile.
+        /// Can be hierarchical. For example: 'Profile/SubProfile'.
+        /// </summary>
+        string Profile { get; set; }
     }
 }
